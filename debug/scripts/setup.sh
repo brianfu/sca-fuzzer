c8a018082b5dfdc58b9ca104eedc25ead2d248c1
Brian Fu <t-fubo@microsoft.com>
2024-07-30 12:54:53 +0000

    removed debug dir

--- debug/scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-# Add new unknown deps to pyproject.toml:dependencies
-
-python3 -m pip install --upgrade pip
-
-# Revizor
-pip install scipy xxhash pyelftools numpy unicorn==1.0.3 
-
-# CI testing (tests/runtests.py)
-pip install types-PyYAML mypy flake8
